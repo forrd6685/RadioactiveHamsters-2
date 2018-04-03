3a468373b5948376bbd6c39d85fd103e3dab19f3
Devin Forrest <forrd6685@WRDSB.CA>
2018-04-03 10:59:18 -0400

    Two Ghosts

---
+++ Code/Scratches/Multiple Ghosts/desktop/src/com/gdx/twoghosts/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gdx.twoghosts.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gdx.twoghosts.TwoGhosts;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TwoGhosts(), config);
+	}
+}
