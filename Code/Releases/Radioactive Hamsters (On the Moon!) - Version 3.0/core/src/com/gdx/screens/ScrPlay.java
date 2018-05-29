76e8a2d292d04699ffeb625c5f0c42d9c35c36eb
Bradley Vachon <vachb8648@WRDSB.CA>
2018-05-29 09:58:36 -0400

    Version 3.0 (Cleaned up Render)

--- Code/Releases/Radioactive Hamsters (On the Moon!) - Version 3.0/core/src/com/gdx/screens/ScrPlay.java
+++ Code/Releases/Radioactive Hamsters (On the Moon!) - Version 3.0/core/src/com/gdx/screens/ScrPlay.java
@@ -9,20 +9,16 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.gdx.common.SprMartian;
-import com.gdx.common.SprHamster;
-import com.gdx.common.SprMap;
-import com.gdx.common.SprNuke;
+import com.gdx.common.*;
 import com.gdx.hamsters.GamHamsters;
-import com.gdx.common.SprPlayableMartian;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class ScrPlay implements Screen, InputProcessor {
     SpriteBatch batch;
-    int nGhostdX, nGhostdY, nWantMoveHam, nWantMoveMar, nMarFrame, nHamFrame, nY;
-    boolean bMovement, bIsHit, bRadioactive;
+    int nGhostdX, nGhostdY, nWantMoveHam, nWantMoveMar, nMarFrame, nHamFrame, nY, nArrowMove;
+    boolean bMovement, bIsHit, bExists;
     OrthographicCamera ocCam;
     ArrayList<SprMartian> arSprMartians = new ArrayList<SprMartian>();
     SprHamster sprHamster;
@@ -55,22 +51,7 @@ public class ScrPlay implements Screen, InputProcessor {
 
     @Override
     public void show() {
-        for (SprMartian sp : arSprMartians) {
-            sp.reset();
-        }
-        bRadioactive = false;
-        if (gamHamsters.bMultiplayer) {
-            sprPMart = new SprPlayableMartian(215, 229, 15, 15);
-            sprPMart.reset();
-        } else {
-            arSprMartians.add(new SprMartian(215, 229, 15, 15));
-        }
-        for (SprMartian sp : arSprMartians) {
-            sp.reset();
-        }
-        sprHamster.reset();
-        nWantMoveHam = 0;
-        sprNuke.bShow = false;
+        reset();
         try {
             sprMap.mapMaker();
         } catch (FileNotFoundException e) {
@@ -88,11 +69,44 @@ public class ScrPlay implements Screen, InputProcessor {
         Gdx.gl.glClearColor(255, 255, 255, 0);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.setProjectionMatrix(ocCam.combined);
-        if(bRadioactive) {
-            if ((int) (Math.random() * 30 + 1) == 1) {
-                sprHamster.statusUpdate(2);
-            }
-        }
+        frames();
+        allMovement();
+        hitTests();
+        batch.begin();
+        drawAll();
+//        batch.draw(sprBackground, 0, 0);
+//        sprHamster.sprTemp.draw(batch);
+//        if (gamHamsters.bMultiplayer) {
+//            sprPMart.animation(nMarFrame);
+//            batch.draw(sprPMart.sprTemp, sprPMart.getX(), sprPMart.getY(), sprPMart.getWidth(), sprPMart.getHeight());
+//        }
+//        for (SprMartian sp : arSprMartians) {
+//            batch.draw(sp.sprTemp, sp.getX(), sp.getY(), sp.getWidth(), sp.getHeight());
+//        }
+//        if (sprNuke.bShow) {
+//            sprNuke.draw(batch);
+//        }
+//        sprMap.draw(batch);
+//        if (sprMap.alSprPellets.isEmpty()) {
+//            sprHamster.statusUpdate(1);
+//        }
+//        for (SprPellet sp : sprMap.alSprPellets) {
+//            sp.draw(batch);
+//        }
+////        for (int nI = 0; nI < sprMap.alSprMainWalls.size(); nI++) {
+////            sprMap.alSprMainWalls.get(nI).draw(batch);
+////        }
+////        for (int nI = 0; nI < sprMap.alSprGhostHouse.size(); nI++) {
+////            sprMap.alSprGhostHouse.get(nI).draw(batch);
+////        }
+        batch.end();
+    }
+
+    public boolean isHit(Sprite spr1, Sprite spr2) {
+        return spr1.getBoundingRectangle().overlaps(spr2.getBoundingRectangle());
+    }
+
+    public void frames() {
         nMarFrame++;
         nHamFrame++;
         if (nMarFrame > 8) {
@@ -101,73 +115,59 @@ public class ScrPlay implements Screen, InputProcessor {
         if (nHamFrame > 12) {
             nHamFrame = 0;
         }
+    }
+
+    public void allMovement() {
         for (SprMartian sp : arSprMartians) {
-            sp.move();
-            sprMap.gHitWall(sp);
-            sprMap.warpingEdge(sp);
+            sp.movement(sprMap, nMarFrame);
         }
         if (gamHamsters.bMultiplayer) {
-            sprPMart.move(nWantMoveMar, sprMap);
-            sprMap.pHitWall(sprPMart);
-            sprMap.warpingEdge(sprPMart);
+            sprPMart.movement(nWantMoveMar, sprMap);
         }
-        sprHamster.move(nWantMoveHam, sprMap);
-        sprMap.hHitWall(sprHamster);
-        sprMap.warpingEdge(sprHamster);
+        sprHamster.movement(nWantMoveHam, sprMap, nHamFrame);
+    }
+
+    public void hitTests() {
         for (SprMartian sp : arSprMartians) {
             bIsHit = isHit(sprHamster, sp);
-            if (bIsHit) {
-                gamHamsters.updateState(2);
-            }
+            if (bIsHit) gamHamsters.updateState(2);
         }
         if (gamHamsters.bMultiplayer) {
             bIsHit = isHit(sprHamster, sprPMart);
-            if (bIsHit) {
-                gamHamsters.updateState(2);
-            }
+            if (bIsHit) gamHamsters.updateState(2);
         }
         bIsHit = isHit(sprHamster, sprNuke);
-        if (bIsHit) {
-            gamHamsters.updateState(3);
+        if (bIsHit) gamHamsters.updateState(3);
+        for (SprPellet sp : sprMap.alSprPellets) sp.updateStatus(sprHamster);
+        if (sprMap.alSprPellets.isEmpty()) {
+            for (int nI = 0; nI < sprMap.alSprGhostHouse.size(); nI++) {
+                sprMap.alSprGhostHouse.remove(nI);
+            }
+            sprNuke.bShow = true;
         }
-        batch.begin();
+    }
+
+    public void drawAll(){
         batch.draw(sprBackground, 0, 0);
-        sprHamster.animation(nHamFrame);
         sprHamster.sprTemp.draw(batch);
         if (gamHamsters.bMultiplayer) {
             sprPMart.animation(nMarFrame);
             batch.draw(sprPMart.sprTemp, sprPMart.getX(), sprPMart.getY(), sprPMart.getWidth(), sprPMart.getHeight());
         }
         for (SprMartian sp : arSprMartians) {
-            sp.animation(nMarFrame);
             batch.draw(sp.sprTemp, sp.getX(), sp.getY(), sp.getWidth(), sp.getHeight());
         }
         if (sprNuke.bShow) {
             sprNuke.draw(batch);
         }
-        if (sprMap.alSprPellets.isEmpty()) {
-            for (int nI = 0; nI < sprMap.alSprGhostHouse.size(); nI++) {
-                sprMap.alSprGhostHouse.remove(nI);
-                bRadioactive = true;
-            }
-            sprNuke.bShow = true;
-        }
-
         sprMap.draw(batch);
-        for (int nI = 0; nI < sprMap.alSprPellets.size(); nI++) {
-            if (isHit(sprHamster, sprMap.alSprPellets.get(nI))) {
-                sprMap.alSprPellets.get(nI).isEaten = true;
-            }
-            if (sprMap.alSprPellets.get(nI).isEaten == true) {
-                sprMap.alSprPellets.remove(nI);
-            } else {
-                sprMap.alSprPellets.get(nI).draw(batch);
-                sprMap.alSprPellets.get(nI).isEaten = false;
-            }
-            if (sprMap.alSprPellets.isEmpty()) {
-                sprHamster.statusUpdate(1);
-
-            }
+        if (sprMap.alSprPellets.isEmpty()) {
+            sprHamster.statusUpdate(1);
+        }
+        for (int nI=0; nI<sprMap.alSprPellets.size(); nI++) {
+            SprPellet sp = sprMap.alSprPellets.get(nI);
+            if (!sp.isEaten) sp.draw(batch);
+            else sprMap.alSprPellets.remove(sp);
         }
 //        for (int nI = 0; nI < sprMap.alSprMainWalls.size(); nI++) {
 //            sprMap.alSprMainWalls.get(nI).draw(batch);
@@ -175,11 +175,24 @@ public class ScrPlay implements Screen, InputProcessor {
 //        for (int nI = 0; nI < sprMap.alSprGhostHouse.size(); nI++) {
 //            sprMap.alSprGhostHouse.get(nI).draw(batch);
 //        }
-        batch.end();
     }
 
-    public boolean isHit(Sprite spr1, Sprite spr2) {
-        return spr1.getBoundingRectangle().overlaps(spr2.getBoundingRectangle());
+    public void reset() {
+        if (arSprMartians.size() > 3) {
+            arSprMartians.remove(arSprMartians.size() - 1);
+        }
+        if (gamHamsters.bMultiplayer) {
+            sprPMart = new SprPlayableMartian(215, 229, 15, 15);
+            sprPMart.reset();
+        } else {
+            arSprMartians.add(new SprMartian(215, 229, 15, 15));
+        }
+        for (SprMartian sp : arSprMartians) {
+            sp.reset();
+        }
+        sprHamster.reset();
+        nWantMoveHam = 0;
+        sprNuke.bShow = false;
     }
 
     @Override
@@ -214,16 +227,22 @@ public class ScrPlay implements Screen, InputProcessor {
         } else if (keycode == Input.Keys.A) {
             nWantMoveHam = 4;
         }
-
-        if (gamHamsters.bMultiplayer) {
-            if (keycode == Input.Keys.UP) {
-                nWantMoveMar = 1;
-            } else if (keycode == Input.Keys.RIGHT) {
-                nWantMoveMar = 2;
-            } else if (keycode == Input.Keys.DOWN) {
-                nWantMoveMar = 3;
-            } else if (keycode == Input.Keys.LEFT) {
-                nWantMoveMar = 4;
+        if (keycode == Input.Keys.UP) {
+            nArrowMove = 1;
+        } else if (keycode == Input.Keys.RIGHT) {
+            nArrowMove = 2;
+        } else if (keycode == Input.Keys.DOWN) {
+            nArrowMove = 3;
+        } else if (keycode == Input.Keys.LEFT) {
+            nArrowMove = 4;
+        } else {
+            nArrowMove = 0;
+        }
+        if (nArrowMove != 0) {
+            if (gamHamsters.bMultiplayer) {
+                nWantMoveMar = nArrowMove;
+            } else {
+                nWantMoveHam = nArrowMove;
             }
         }
         if (keycode == Input.Keys.ALT_RIGHT) {
